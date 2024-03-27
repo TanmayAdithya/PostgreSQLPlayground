a9cefa930d95ceff5a60e7a5ce1be0c8633b7d94
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-03-27 22:28:36 +0530

    render visited countries

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -1,14 +1,33 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import pg from 'pg';
+
+process.loadEnvFile();
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: process.env.pass,
+  port: 5432,
+});
+db.connect();
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  const result = await db.query('SELECT country FROM visited_countries');
+  let countries = [];
+
+  result.rows.forEach((country) => {
+    countries.push(country.country);
+  });
+
+  res.render('index.ejs', { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
